2d8c2f78e83fc832f66ef19392f8a27661559330
alisa-uthi <alisauthi@gmail.com>
2023-06-09 17:36:07 +0700

    add chat message section

---
+++ src/app/shared/components/send-message-input/send-message-input.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-send-message-input',
+  templateUrl: './send-message-input.component.html',
+  styleUrls: ['./send-message-input.component.scss']
+})
+export class SendMessageInputComponent implements OnInit {
+
+  constructor(private fb: FormBuilder) { }
+
+  form = this.fb.group({
+    message: ['', Validators.required]
+  });
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,11 +4,13 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
 import { ChatHistoryTitlesComponent } from "./components/chat-history-titles/chat-history-titles.component";
 import { DividerComponent } from "./components/divider/divider.component";
+import { SendMessageInputComponent } from "./components/send-message-input/send-message-input.component";
 
 @NgModule({
     declarations: [
         ChatHistoryTitlesComponent,
         DividerComponent,
+        SendMessageInputComponent
     ],
     imports: [
         MaterialModule,
@@ -20,8 +22,9 @@ import { DividerComponent } from "./components/divider/divider.component";
         MaterialModule,
         ChatHistoryTitlesComponent,
         DividerComponent,
+        SendMessageInputComponent
     ],
     providers: [],
-  })
-  export class SharedModule { }
+})
+export class SharedModule { }
   
